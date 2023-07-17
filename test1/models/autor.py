509a9ea2264ed5d46a35e3a444858f99e9070af5
ShinhyoBelok <shibo6937@gmail.com>
2023-07-17 10:18:23 -0700

    _rec_name

--- test1/models/autor.py
+++ test1/models/autor.py
@@ -1,8 +1,11 @@
 from odoo import models, fields
 
+
 # estamos creando uan tabla en la base de datos
 class Autor(models.Model):
-    _name = 'autores' #nombre de la tabla
+    _name = 'autores'  # nombre de la tabla
+    # rec_name cambia el field que regresa un many2one de este modelo, que por default es name en este caso ahora va a reqresar last_name
+    _rec_name = 'last_name'
 
     name = fields.Char(string="Nombre del autor", required=True)
     last_name = fields.Char(string="Apellido")
